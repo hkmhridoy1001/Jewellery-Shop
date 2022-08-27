25fd458aca0186d0b734c2ca09df407be7e22b3a
Hridoy Mohonto <112055398+hkmhridoy1001@users.noreply.github.com>
2022-08-27 17:13:43 +0600

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// tesimonial section 
+
+
+
+$('.r_js').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    // dots: true,
+    responsive: [
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+  });
